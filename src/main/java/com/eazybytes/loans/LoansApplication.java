4f51f22f359982a851ea01863e6157013e1e1e2f
Maleesha Sandakalum <maleeshaGitHub0000@gmail.com>
2025-06-29 22:17:39 +0530

    enabling eureca server

--- src/main/java/com/eazybytes/loans/LoansApplication.java
+++ src/main/java/com/eazybytes/loans/LoansApplication.java
@@ -4,8 +4,10 @@ import com.eazybytes.loans.dto.LoansContactInfoDto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableConfigurationProperties(value = {LoansContactInfoDto.class})
+@EnableDiscoveryClient
 @SpringBootApplication
 public class LoansApplication {
 
